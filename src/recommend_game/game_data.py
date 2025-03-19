337bd08ab28d7b420cdc5a27b38c40acea742e32
eth3r3aI <adam.caojz@gmail.com>
2025-03-19 15:06:55 -0400

    adding game category

---
+++ src/recommend_game/game_data.py
@@ -0,0 +1,11 @@
+game_categories = {
+    "RPG": ["The Witcher 3", "Skyrim", "Dark Souls", "Elden Ring", "Cyberpunk 2077", "Fallout", "Bloodborne"],
+    "Shooter": ["Halo", "Call of Duty", "Doom", "Half-Life"],
+    "Horror": ["Resident Evil", "The Last of Us"],
+    "Platformer": ["Sonic the Hedgehog", "Super Mario Bros"],
+    "Open-World/Action-Adventure": ["The Legend of Zelda", "Red Dead Redemption 2", "Grand Theft Auto V"],
+    "Puzzle": ["Portal"],
+    "Sandbox": ["Minecraft"]
+}
+
+print(game_categories)
