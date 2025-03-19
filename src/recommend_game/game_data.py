388abdd5ddf050dc25ae4593abbecf3b25483dc7
eth3r3aI <adam.caojz@gmail.com>
2025-03-19 15:25:03 -0400

    reformatted

--- src/recommend_game/game_data.py
+++ src/recommend_game/game_data.py
@@ -1,11 +1,25 @@
+"""This module provides data for recommend_game.py module"""
+
 game_categories = {
-    "RPG": ["The Witcher 3", "Skyrim", "Dark Souls", "Elden Ring", "Cyberpunk 2077", "Fallout", "Bloodborne"],
+    "RPG": [
+        "The Witcher 3",
+        "Skyrim",
+        "Dark Souls",
+        "Elden Ring",
+        "Cyberpunk 2077",
+        "Fallout",
+        "Bloodborne",
+    ],
     "Shooter": ["Halo", "Call of Duty", "Doom", "Half-Life"],
     "Horror": ["Resident Evil", "The Last of Us"],
     "Platformer": ["Sonic the Hedgehog", "Super Mario Bros"],
-    "Open-World/Action-Adventure": ["The Legend of Zelda", "Red Dead Redemption 2", "Grand Theft Auto V"],
+    "Open-World/Action-Adventure": [
+        "The Legend of Zelda",
+        "Red Dead Redemption 2",
+        "Grand Theft Auto V",
+    ],
     "Puzzle": ["Portal"],
-    "Sandbox": ["Minecraft"]
+    "Sandbox": ["Minecraft"],
 }
 
 print(game_categories)
