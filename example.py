9852cbbb29e78e3df848ed328c1910b7a527db0c
PengJiang-Victor <pj2196@nyu.edu>
2025-03-20 09:33:21 -0400

    modify example.py and add example usage in readme

--- example.py
+++ example.py
@@ -73,7 +73,7 @@ builtins.input = original_input
 print("\n📚 Fun Facts About Games:\n")
 
 game_titles = [
-    "Elden Ring",
+    "Elden",
     "Minecraft",
     "Cyberpunk 2077",
     "Super Mario Bros.",
